9c47178c86485a7d857a91a90b6316c8fa56abd4
hiltonfarias <hilton.farias.da.silva@gmail.com>
2021-03-26 18:46:32 -0300

    implementing application controller and documentation with swagger

---
+++ src/main/java/com/github/hiltonfarias/beerstock/controller/BeerController.java
@@ -0,0 +1,52 @@
+package com.github.hiltonfarias.beerstock.controller;
+
+import com.github.hiltonfarias.beerstock.dto.BeerDTO;
+import com.github.hiltonfarias.beerstock.dto.QuantityDTO;
+import com.github.hiltonfarias.beerstock.exception.BeerAlreadyRegisteredException;
+import com.github.hiltonfarias.beerstock.exception.BeerNotFoundException;
+import com.github.hiltonfarias.beerstock.exception.BeerStockExceededException;
+import com.github.hiltonfarias.beerstock.service.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/beers")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class BeerController implements BeerControllerDocs {
+
+    private final BeerService beerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Override
+    public BeerDTO createBeer(@RequestBody @Valid BeerDTO beerDTO) throws BeerAlreadyRegisteredException {
+        return beerService.createBeer(beerDTO);
+    }
+
+    @GetMapping("/{name}")
+    @Override
+    public BeerDTO findByName(@PathVariable String name) throws BeerNotFoundException {
+        return beerService.findByName(name);
+    }
+
+    @GetMapping
+    @Override
+    public List<BeerDTO> listBeers() {
+        return beerService.listAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @Override
+    public void deleteById(@PathVariable Long id) throws BeerNotFoundException {
+        beerService.deleteById(id);
+    }
+
+    public BeerDTO increment(Long id, QuantityDTO quantityDTO) throws BeerNotFoundException, BeerStockExceededException {
+        return beerService.increment(id, quantityDTO.getQuantity());
+    }
+}
